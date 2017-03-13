32fa00d1567fdf85188d92a3151b33a3d37ce1d7
pengxiaofeng <qhpbdt66@gmail.com>
2017-03-13 19:58:32 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
     end
     devise_for :users
     root 'products#index'
-    resources :products
+
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+
 end
